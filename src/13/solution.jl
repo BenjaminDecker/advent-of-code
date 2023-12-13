d81d554d023c499ddf9ce3ab77c39a8b56587fc4
Benjamin Decker <benjamin.decker@tum.de>
2023-12-13 15:25:15 +0100

    Simplify code for day 13

--- src/13/solution.jl
+++ src/13/solution.jl
@@ -1,21 +1,5 @@
 
-function vertical_reflection(pattern)
-    _, cols = size(pattern)
-    for col in 1:(cols-1)
-        issymmetric = true
-        for offset in 1:(min(col, cols - col))
-            if pattern[:, col-offset+1] != pattern[:, col+offset]
-                issymmetric = false
-                break
-            end
-        end
-        if issymmetric
-            return col
-        end
-    end
-end
-
-function vertical_smudge_reflection(pattern)
+function vertical_reflection(pattern::Matrix{Char}, num_smudes::Int)::Union{Int,Nothing}
     rows, cols = size(pattern)
     for col in 1:(cols-1)
         smudges = 0
@@ -25,24 +9,23 @@ function vertical_smudge_reflection(pattern)
                 1:rows
             )
         end
-        if smudges == 1
+        if smudges == num_smudes
             return col
         end
     end
 end
 
-function point_of_incidence(pattern::Matrix{Char}, pt2::Bool)::Int
-    fun = pt2 ? vertical_smudge_reflection : vertical_reflection
-    v_reflection = fun(pattern)
+function point_of_incidence(pattern::Matrix{Char}, num_smudged::Int)::Int
+    v_reflection = vertical_reflection(pattern, num_smudged)
     if !isnothing(v_reflection)
         return v_reflection
     end
-    return 100 * fun(permutedims(pattern, (2, 1)))
+    return 100 * vertical_reflection(permutedims(pattern, (2, 1)), num_smudged)
 end
 
 function point_of_incidence(lines::String, pt2::Bool)::Int
     return mapreduce(
-        pattern -> point_of_incidence(pattern, pt2),
+        pattern -> point_of_incidence(pattern, pt2 ? 1 : 0),
         +,
         map(
             pattern -> stack(filter(!isempty, map(collect, split(pattern, '\n'))); dims=1),
