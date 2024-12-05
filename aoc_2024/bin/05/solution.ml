648b5d50d86f38c4274acd35993201a55f4a5c77
Benjamin Decker <benjamin.decker@tum.de>
2024-12-05 20:40:22 +0100

    Day 05, Part 2

--- aoc_2024/bin/05/solution.ml
+++ aoc_2024/bin/05/solution.ml
@@ -30,26 +30,37 @@ let isCorrect rules update =
     | (true, forbidden) -> (not (List.exists ((=) elem) forbidden)), (rules.(elem) @ forbidden)
   ) (true, []) (List.rev update))
 
-let sol01 lines = 
-  let ruleLines = lines |> List.filter (String.exists ((=) '|')) in
-  let updateLines = lines |> List.filter (String.exists ((=) ',')) in
-  let rules = getRules ruleLines in
-  updateLines
-  |> List.map parseUpdate
-  |> List.filter (isCorrect rules)
-  |> List.map (fun l -> List.nth l ((List.length l)/2))
+let smallerEqual rules lhs rhs = 
+  Bool.to_int (List.exists ((=) rhs) rules.(lhs))
+;;
+
+let createSum updates = 
+  updates
+  |> List.map (fun update -> List.nth update ((List.length update)/2))
   |> List.fold_left (+) 0
-;;
 
-let sol02 _ = 
-  0
-;;
+let sol lines = 
+  let ruleLines = lines |> List.filter (String.exists ((=) '|')) in
+  let updateLines = lines |> List.filter (String.exists ((=) ',')) |> List.map parseUpdate in
+  let rules = getRules ruleLines in
 
+  let sol01 = updateLines
+  |> List.filter (isCorrect rules)
+  |> createSum in
+
+  let sol02 = updateLines
+  |> List.filter (fun update -> not (isCorrect rules update))
+  |> List.map (List.sort (smallerEqual rules))
+  |> createSum in
+
+  sol01, sol02
+;;
 
 let () = 
   let lines = read_lines "bin/05/input.txt" in
+  let (sol01, sol02) = lines |> sol in
   print_endline "Solution 1:";
-  lines |> sol01 |> print_int |> print_newline;
+  sol01 |> print_int |> print_newline;
   print_endline "Solution 2:";
-  lines |> sol02 |> print_int |> print_newline;
+  sol02 |> print_int |> print_newline;
 ;;
