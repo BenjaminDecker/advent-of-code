e6cf2d657b0ccc7de2f0dbaf01b316104861042d
Benjamin Decker <benjamin.decker@tum.de>
2024-12-10 15:09:00 +0100

    Day 10 Part 2

--- aoc_2024/bin/10/solution.ml
+++ aoc_2024/bin/10/solution.ml
@@ -12,6 +12,18 @@ let rec count_paths heightmap position =
   |> Seq.fold_left (@) []
 ;;
 
+let rec count_paths_pt2 heightmap position =
+  let current_height = heightmap#get_at position in
+  if current_height == 9 then 1 else
+  [N;E;S;W]
+  |> List.to_seq
+  |> (Seq.map (next_coords position))
+  |> Seq.filter heightmap#is_valid
+  |> Seq.filter (fun coord -> heightmap#get_at coord == (current_height + 1))
+  |> Seq.map (count_paths_pt2 heightmap)
+  |> Seq.fold_left (+) 0
+;;
+
 let sol01 lines = 
   let lines = 
     lines 
@@ -24,11 +36,23 @@ let sol01 lines =
   heightmap#find_all_coords ((=) 0) |> Seq.map (count_paths heightmap) |> Seq.map (List.sort_uniq smallerEqual) |> Seq.map List.length |> Seq.fold_left (+) 0
 ;;
 
+let sol02 lines = 
+  let lines = 
+    lines 
+    |> List.map String.to_seq 
+    |> List.map (Seq.map int_of_char) 
+    |> List.map (Seq.map (fun c -> c - (int_of_char '0')))
+    |> List.map List.of_seq
+  in
+  let heightmap = new flat_array lines in
+  heightmap#find_all_coords ((=) 0) |> Seq.map (count_paths_pt2 heightmap) |> Seq.fold_left (+) 0
+;;
+
 let () = 
   Printexc.record_backtrace true;
   let lines = read_lines "bin/10/input.txt" in
   print_endline "Solution 1:";
   sol01 lines |> print_int |> print_newline;
   print_endline "Solution 2:";
-  (* sol02 lines |> print_int |> print_newline; *)
+  sol02 lines |> print_int |> print_newline;
 ;;
